9b544339aa16f06dba2967d0f065bfc454daf215
kdrl <kdrl7@naver.com>
2016-09-01 16:28:09 +0900

    fixed some bug

---
+++ deploy/public/app/path.service.js
@@ -0,0 +1,26 @@
+app.factory('pathService', Service);
+
+function Service($http) {
+    var service = {};
+
+    service.findPath = findPath;
+    
+    return service;
+
+    function findPath(sendData) {
+      console.log("findPath 발동");
+        return $http.post('/api/path/findpath',sendData).then(handleSuccess, handleError);
+    }
+    
+    function handleSuccess(res) {
+        console.log("handleSuccess")
+        console.log(res.data)
+        return res.data;
+    }
+
+    function handleError(res) {
+        console.log("handleError")
+        console.log(res.data)
+        return res.data;
+    }
+}
