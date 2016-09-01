06adb155da9f33ee9022dca73d188488aedf5f3e
pachicourse <ddds78199500825@gmail.com>
2016-09-01 17:35:31 +0900

    Add files via upload

--- raspberry/pi_1.js
+++ raspberry/pi_1.js
@@ -1,52 +1,59 @@
-var key = require("./config.json");
-// key.iftttKey -> ifttt keyです
+var express = require('express');
+var request = require('request');
+var app = express();
+var bodyParser = require('body-parser');
 
-//apiサーバー
-var target = 'http://192.168.12.5:5000';
+var key = require("./config.json"); 
+
+key.eventName = "IoT_Intern";
+
+app.set('port', (process.env.PORT || 5000));
+ 
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
 
 // 8番のGPIOピンを出力として登録
-var fs = require('fs'); 
+var fs = require('fs')
 fs.writeFileSync('/sys/class/gpio/unexport', '8');
 fs.writeFileSync('/sys/class/gpio/export', '8');
 fs.writeFileSync('/sys/class/gpio/gpio8/direction', 'out');
 
+ 
 function blink(){
-	console.log("blink!");
-	fs.writeFileSync('/sys/class/gpio/gpio8/value', '0');
-	fs.writeFileSync('/sys/class/gpio/gpio8/value', '1');
-	setTimeout(function(){
-		fs.writeFileSync('/sys/class/gpio/gpio8/value', '0');
-	},3000);
+    console.log("blink!");
+    fs.writeFileSync('/sys/class/gpio/gpio8/value', '0');
+    fs.writeFileSync('/sys/class/gpio/gpio8/value', '1');
+    setTimeout(function(){
+    fs.writeFileSync('/sys/class/gpio/gpio8/value', '0');
+    },3000);
 }
 
-
-var express = require('express');
-var bodyParser = require('body-parser');
-var app = express();
-var http = require('http');
-var port = 5000;
-var server = http.createServer(app);
-var request = require('request');
-
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-
-function relaySend(message){
-	console.log("relaySend!");
-	request.post({url:target, form: {message:message}}, function(err,httpResponse,body){ 
-	console.log(err);
-	 });
-}
-
-app.post('/', function(req, res) {
-	console.log("post");
-	console.dir(req.body.message);
-	if(req.body.message){
-		blink();
-		relaySend(req.body.message);	
-	}
+ 
+// IFTTTへ送信
+app.post('/', function(request, response) {
+    blink();
+    console.log(request.body.message);
+    var _request = require('request'); 
+    var options = {
+        uri: 'http://maker.ifttt.com/trigger/' + key.eventName + '/with/key/' + key.iftttKey,
+        form: {
+            value1:request.body.message
+        },
+        json: true
+    };
+ 
+    console.log('---------- [output]');
+    console.log(options);
+ 
+    _request.post(options, function(error, response, body){
+        if (!error && response.statusCode == 200) {
+            console.log(body);
+        } else {
+            console.log('error: '+ response.statusCode);
+        }
+    });
 });
-
-server.listen(port,function(){
-    console.log("server listening on port : ", port)
+ 
+app.listen(app.get('port'), function() {
+    console.log('Node app is running on port', app.get('port'));
 });
